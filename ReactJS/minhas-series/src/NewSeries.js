5ee3e356a9cc6cc69c71bce88c9b08bf12dbd919
EGGabriel <ps.eggabriel@gmail.com>
2017-09-04 18:46:14 -0300

    add React Files

---
+++ ReactJS/minhas-series/src/NewSeries.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import api from './Api'
+import { Redirect } from 'react-router-dom'
+
+const statuses = {
+    'watched': 'Assistido',
+    'Watching': 'Assistindo',
+    'toWatch': 'Assistir',
+}
+
+class NewSeries extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            genres: [],
+            isLoading: false,
+            redirect: false
+        }
+        this.saveSeries = this.saveSeries.bind(this)
+    }
+    componentDidMount() {
+        this.setState({ isLoading: true })
+        api.loadGenres().then((res) => {
+            this.setState({
+                isLoading: false,
+                genres: res.data
+            })
+        })
+    }
+    saveSeries() {
+        const NewSeries = {
+            name: this.refs.name.value,
+            status: this.refs.status.value,
+            genre: this.refs.genre.value,
+            comment: this.refs.comment.value,
+        }
+        api.saveSeries(NewSeries)
+            .then((res) => {
+                this.setState({
+                    redirect: '/series/' + this.refs.genre.value
+                })
+            })
+    }
+    render() {
+        return (
+            <section className="intro-section">
+                {this.state.redirect && <Redirect to={this.state.redirect} />}
+                <h1> Nova Série </h1>
+                <form>
+                    Nome: <input type="text" ref='name' className="form-control" /><br />
+                    Status:
+                    <select ref='status'>
+                        {Object.keys(statuses).map(key => <option key={key} value={key}>{statuses[key]}</option>)}
+                    </select><br />
+                    Genre:
+                    <select ref='genre'>
+                        {this.state.genres.map(key => <option key={key} value={key}>{key}</option>)}
+                    </select><br />
+                    Comentários: <textarea ref='comment' type="text" className="form-control" /><br />
+                    <button type="button" onClick={this.saveSeries}>Salvar!</button>
+                </form>
+            </section>
+        )
+    }
+}
+
+export default NewSeries
